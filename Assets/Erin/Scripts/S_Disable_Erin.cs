571ad01521ff67149112fafa93ea6d387dd0f125
ErinScribner13 <erinscrib@gmail.com>
2024-06-30 07:55:02 -0700

    Kitten Update
    Commented code
    
    Kitten is now dropped below the player, not to the right of the player
    
    Picked up kitten is now in the general mouth area of the player. Still needs some refining

--- Assets/Erin/Scripts/S_Disable_Erin.cs
+++ Assets/Erin/Scripts/S_Disable_Erin.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/* Author: Erin Scribner
+ * 
+ * Date: 6/30/2024
+ * 
+ * Description: When game starts up, disables the gameObject's functionality.
+ *              Used primarely to hide menus at the start of the game
+ * 
+ * Public Functions: None
+ * 
+ * Other Scripts Needed: None
+ */
 public class S_Disable_Erin : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /*
+     * At the start of the game deactivate the pause menu. 
+     * This hides the pause menu at the start of the game
+     */
     void Start()
     {
         gameObject.SetActive(false);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 }
